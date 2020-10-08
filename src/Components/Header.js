922ff4e63b0af8853a0288f0ea9cf9fe4a695a61
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-08 13:57:25 -0400

    Unit 2 Project

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,20 +6,16 @@ const Header = (props) => {
     <div className="Header">
       <nav className="navBar">
         <Link to="/Home">
-          {/* <a id="HomeButton">Home</a> */}
           <img id="HomeButton" src="https://i.imgur.com/y3itP3As.jpg" alt="" />
         </Link>
 
         <Link to="/Songs">
-          {/* <a id="SongsButton">Songs</a> */}
           <img id="SongsButton" src="https://i.imgur.com/ZuoBHdrs.jpg" alt="" />
         </Link>
         <Link to="/Play">
-          {/* <a id="PlayButton">Play</a> */}
           <img id="PlayButton" src="https://i.imgur.com/lAUbiUYt.jpg" alt="" />
         </Link>
         <Link to="/ArtistInformation">
-          {/* <a id="ArtistButton">Artist</a> */}
           <img
             id="ArtistButton"
             src="https://i.imgur.com/VSv1Gpks.jpg"
